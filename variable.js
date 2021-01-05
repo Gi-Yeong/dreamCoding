b69ad789581299f9482f22c773225b5d1d407ec8
kiyoung85 <kiyoung85@gmail.com>
2021-01-05 21:23:10 +0900

    variable 내용 추가

--- variable.js
+++ variable.js
@@ -3,7 +3,7 @@
 // added ECMAScript 5
 'use strict';
 
-// 2. Variable
+// 2. Variable, rw(read/write)
 // let (added in ES6)
 let globalName = 'global name';
 {
@@ -25,13 +25,19 @@ console.log(globalName);
 }
 console.log(age);
 
-// 3. Constant
+// 3. Constant, r(read only)
+// use const whenever possible.
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const manNumber = 5;
+
+// Note!
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 // - security
 // - thread safety
 // - reduce human mistakes
-const daysInWeek = 7;
-const manNumber = 5;
 
 // 4. Variable types
 // primitive, single item : number, string, boolean, null, undefined, symbol
